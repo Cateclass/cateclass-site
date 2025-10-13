5e529f9c93da09992cf69e7e54470de80e79cb55
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-10-13 16:47:49 -0300

    Adicionando | sidebar

---
+++ app/views/coordenacao/dashboard.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coordenação | Dashboard</title>
+
+    <!-- TailwindCSS CLI -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    
+</head>
+<body class="bg-[#E5ECFF]">
+
+    <!-- Sidebar -->
+    <aside class="bg-[#fff] w-[300px] h-[100vh]">
+
+        <!-- "header" -->
+        <div class="py-[30px] mb-[30px] border-b-1 border-black">
+            <img src="localhost/projetos/cateclass-site/app/views/assets/img/logo-no-bg.png" alt="">
+            <p>Cateclass</p>
+            <span>Plataforma Educacional</span>
+        </div>
+
+        <!-- Links de navegação -->
+        <nav class="mb-[30px]">
+
+            <ul class="list-none">
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Turmas</a></li>
+                <li><a href="#">Catequistas</a></li>
+                <li><a href="#">Crismandos</a></li>
+                <li><a href="#">1ª Eucaristia</a></li>
+                <li><a href="#">Relatórios</a></li>
+            </ul>
+
+        </nav>
+
+        <p class="uppercase">Ações rápidas</p>
+        <a class="block bg-[#008000] text-[#fff] w-[250px] py-[7px] text-center" href="#">Nova Turma</a>
+
+    </aside>
+    
+</body>
+</html>
